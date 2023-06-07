74ab5c3fd6a0e9e6c644f4acfa39a1568c5cc102
VishalBhuse <vbhuse1997@gmail.com>
2023-06-07 15:44:42 +0530

    backend basic struture ready

---
+++ BackEnd/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { connection } = require("./config/db");
+const port = 8000 || process.env.PORT;
+var cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("home");
+});
+
+app.listen(port, async () => {
+  try {
+    await connection;
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("listening to the port " + port);
+});

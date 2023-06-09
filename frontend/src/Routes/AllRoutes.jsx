cf662d61fc435cdd4d296a859d2288c6752a2b6f
VishalBhuse <vbhuse1997@gmail.com>
2023-06-09 13:39:03 +0530

    aroutes updated

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../Component/Login";
 import Signup from "../Component/Signup";
+import RequireAuth from "../HOC/RequiredAuth";
+import AllInventory from "../Page/AllInventory";
 import Home from "../Page/Home";
+import Inventory from "../Page/Inventory";
+import SingleInventory from "../Page/SingleInventory";
+import Unauth from "../Page/Unauth";
 
 const AllRoutes = () => {
   return (
@@ -11,6 +16,18 @@ const AllRoutes = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/addcar"
+          element={
+            <RequireAuth child>
+              <Inventory />
+            </RequireAuth>
+          }
+        />
+        <Route path="/inventoryall" element={<AllInventory />} />
+        <Route path="/singleinventory/:id" element={<SingleInventory />} />
+        <Route path="/unauthor" element={<Unauth />} />
+        
       </Routes>
     </>
   );
